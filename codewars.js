0a3080794323f3c25fc810d797e88d4abf155a43
Maksym <gurinmaxim11@gmail.com>
2022-03-20 21:42:09 +0200

    2

--- codewars.js
+++ codewars.js
@@ -32,4 +32,33 @@ describe("Test Cases", function () {
 });
 
 
-//---------------------------------------
+//=======================================
+
+// Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
+
+// Examples (Input -> Output)
+// * 15  -> '101.25 Chinese Yuan'
+// * 465 -> '3138.75 Chinese Yuan'
+// The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers should be represented as a string with 2 decimal places. (e.g. "21.00" NOT "21.0" or "21")
+
+//=======================================
+
+function usdcny(usd) {
+    const CNY = 6.75;
+    let temp = usd * CNY;
+
+    let result = temp.toFixed(2) + ' Chinese Yuan';
+
+    return result;
+}
+
+const chai = require("chai");
+const as = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Basic tests", () => {
+    it("Testing for fixed tests", () => {
+        as.strictEqual(usdcny(15), '101.25 Chinese Yuan');
+        as.strictEqual(usdcny(465), '3138.75 Chinese Yuan');
+    });
+});
