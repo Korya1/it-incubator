1c734daeeebf49c7c793a35ccd8387c9f7cd6897
Maksym <gurinmaxim11@gmail.com>
2022-03-20 12:37:05 +0200

    2

--- freecodecamp.js
+++ freecodecamp.js
@@ -17,3 +17,530 @@ function testSize(num) {
 }
 
 testSize(7);
+
+//=================================
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+    // Only change code below this line
+    if (strokes == 1) {
+        return names[0];
+    } else if (strokes <= par - 2) {
+        return names[1];
+    } else if (strokes === par - 1) {
+        return names[2];
+    } else if (strokes === par) {
+        return names[3];
+    } else if (strokes === par + 1) {
+        return names[4];
+    } else if (strokes === par + 2) {
+        return names[5];
+    } else {
+        return names[6];
+    }
+    // Only change code above this line
+}
+
+console.log(golfScore(5, 4));
+
+//=================================
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    }
+    // Only change code above this line
+    return answer;
+}
+
+caseInSwitch(1);
+
+//=================================
+function switchOfStuff(val) {
+    var answer = "";
+
+    switch (val) {
+        case "a":
+            answer = "apple";
+            break;
+        case "b":
+            answer = "bird";
+            break;
+        case "c":
+            answer = "cat";
+            break;
+        default:
+            answer = "stuff";
+    }
+
+    return answer;
+}
+
+//=================================
+function chainToSwitch(val) {
+    var answer = "";
+    // Only change code below this line
+    switch (val) {
+        case "bob":
+            answer = "Marley";
+            break;
+        case 42:
+            answer = "The Answer";
+            break;
+        case 1:
+            answer = "There is no #1";
+            break;
+        case 99:
+            answer = "Missed me by this much!";
+            break;
+        case 7:
+            answer = "Ate Nine";
+            break;
+    }
+    // Only change code above this line
+    return answer;
+}
+
+chainToSwitch(7);
+
+//=================================
+function isLess(a, b) {
+    // Fix this code
+    return a <= b;
+}
+
+isLess(10, 15);
+
+//=================================
+// Setup
+function abTest(a, b) {
+    // Only change code below this line
+    if (a < 0 || b < 0) {
+        return undefined;
+    }
+
+    // Only change code above this line
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+abTest(2, 2);
+
+//=================================
+let count = 0;
+
+function cc(card) {
+    // Only change code below this line
+    switch (card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++;
+            break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--;
+            break;
+    }
+    if (count > 0) {
+        return count + " Bet";
+    } else {
+        return count + " Hold";
+    }
+    // Only change code above this line
+}
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+//=================================
+const myDog = {
+    // Only change code below this line
+    "name": "Whiskers",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Water", "Dogs"]
+    // Only change code above this line
+};
+
+//=================================
+var testObj = {
+    hat: "ballcap",
+    shirt: "jersey",
+    shoes: "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat; // Change this line
+var shirtValue = testObj.shirt; // Change this line
+
+
+//=================================
+// Setup
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16; // Change this Line
+var player = testObj[playerNumber]; // Change this Line
+
+//=================================
+// Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog.name = "Happy Coder";
+
+//=================================
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "woof";
+
+//=================================
+// Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails;
+
+//=================================
+function phoneticLookup(val) {
+    var result = "";
+    var lookup = {
+        "alpha": "Adams",
+        "bravo": "Boston",
+        "charlie": "Chicago",
+        "delta": "Denver",
+        "echo": "Easy",
+        "foxtrot": "Frank"
+    };
+
+    result = lookup[val];
+    // Only change code above this line
+    return result;
+}
+
+//=================================
+function checkObj(obj, checkProp) {
+    if (obj.hasOwnProperty(checkProp)) {
+        return obj[checkProp];
+    } else {
+        return "Not Found";
+    }
+}
+
+//=================================
+var myMusic = [
+    {
+        artist: "Billy Joel",
+        title: "Piano Man",
+        release_year: 1973,
+        formats: ["CD", "8T", "LP"],
+        gold: true
+    },
+    // Add record here
+    {
+        artist: "Deep Purple",
+        title: "Smoke on the water",
+        release_year: 1976,
+        formats: ["CD", "8T", "LP"]
+    }
+];
+
+//=================================
+// Setup
+var myPlants = [
+    {
+        type: "flowers",
+        list: ["rose", "tulip", "dandelion"]
+    },
+    {
+        type: "trees",
+        list: ["fir", "pine", "birch"]
+    }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1];
+
+//=================================
+// Setup
+var recordCollection = {
+    2548: {
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+        artist: 'Robert Palmer',
+        tracks: []
+    },
+    5439: {
+        albumTitle: 'ABBA Gold'
+    }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+    if (prop !== 'tracks' && value !== "") {
+        records[id][prop] = value;
+    } else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false) {
+        records[id][prop] = [value];
+    } else if (prop === "tracks" && value !== "") {
+        records[id][prop].push(value);
+    } else if (value === "") {
+        delete records[id][prop];
+    }
+    return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+//=================================
+// Setup
+const myArray = [];
+
+// Only change code below this line.
+let i = 5;
+while (i >= 0) {
+    myArray.push(i);
+    i--;
+}
+
+//=================================
+var ourArray = [];
+for (var i = 0; i < 5; i++) {
+    ourArray.push(i);
+}
+
+var myArray = [];
+for (var i = 1; i < 6; i++) {
+    myArray.push(i);
+}
+
+//=================================
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+    ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (var i = 1; i < 10; i += 2) {
+    myArray.push(i);
+}
+//=================================
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+    ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 9; i > 0; i -= 2) {
+    myArray.push(i);
+}
+//=================================
+function multiplyAll(arr) {
+    var product = 1;
+    // Only change code below this line
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            product = product * arr[i][j];
+        }
+    }
+    // Only change code above this line
+    return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+//=================================
+var myArray = [];
+var i = 10;
+
+do {
+    myArray.push(i);
+    i++;
+} while (i <= 10);
+
+//=================================
+function sum(arr, n) {
+    if (n <= 0) {
+        return 0;
+    } else {
+        return sum(arr, n - 1) + arr[n - 1];
+    }
+}
+
+//=================================
+// Setup
+const contacts = [
+    {
+        firstName: "Akira",
+        lastName: "Laine",
+        number: "0543236543",
+        likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+        firstName: "Harry",
+        lastName: "Potter",
+        number: "0994372684",
+        likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+        firstName: "Sherlock",
+        lastName: "Holmes",
+        number: "0487345643",
+        likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+        firstName: "Kristian",
+        lastName: "Vos",
+        number: "unknown",
+        likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+];
+
+function lookUpProfile(name, prop) {
+    for (let x = 0; x < contacts.length; x++) {
+        if (contacts[x].firstName === name) {
+            if (contacts[x].hasOwnProperty(prop)) {
+                return contacts[x][prop];
+            } else {
+                return "No such property";
+            }
+        }
+    }
+    return "No such contact";
+}
+
+lookUpProfile("Akira", "likes");
+
+//=================================
+function randomFraction() {
+    // Only change code below this line.
+    var result = 0;
+
+    while (result === 0) {
+        result = Math.random();
+    }
+
+    return result;
+    // Only change code above this line.
+}
+
+//=================================
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+    // Only change code below this line.
+    return Math.floor(Math.random() * 10);
+}
+
+//=================================
+function randomRange(myMin, myMax) {
+    // Only change code below this line
+    return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+    // Only change code above this line
+}
+
+//=================================
+function convertToInteger(str) {
+    return parseInt(str);
+}
+
+convertToInteger("56");
+
+//=================================
+function convertToInteger(str) {
+    return parseInt(str, 2);
+}
+convertToInteger("10011");
+
+//=================================
+function checkEqual(a, b) {
+    return a === b ? "Equal" : "Not Equal";
+}
+
+//=================================
+function checkEqual(a, b) {
+    return a === b ? "Equal" : "Not Equal";
+}
+
+//=================================
+function checkSign(num) {
+    return num > 0 ? "positive"
+        : num < 0 ? "negative"
+            : "zero";
+}
+
+//=================================
+function countdown(n) {
+    if (n < 1) {
+        return [];
+    } else {
+        const arr = countdown(n - 1);
+        arr.unshift(n);
+        return arr;
+    }
+}
+
+//=================================
+function rangeOfNumbers(startNum, endNum) {
+    if (endNum - startNum === 0) {
+        return [startNum];
+    } else {
+        var numbers = rangeOfNumbers(startNum, endNum - 1);
+        numbers.push(endNum);
+        return numbers;
+    }
+}
+
