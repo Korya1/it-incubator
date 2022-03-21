469501430843205e20d3a30de1d35694f2016a4c
Maksym <gurinmaxim11@gmail.com>
2022-03-21 17:43:59 +0200

    7(freecodecamp)

--- freecodecamp.js
+++ freecodecamp.js
@@ -671,10 +671,46 @@ const stats = {
     mode: 23.87,
     min: -0.75,
     average: 35.85
-  };
-  
-  // Only change code below this line
-  const half = ({max, min}) => (max + min) / 2.0; 
-  // Only change code above this line
-  
+};
+
+// Only change code below this line
+const half = ({ max, min }) => (max + min) / 2.0;
+// Only change code above this line
+
 //=================================
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+    // Only change code below this line
+    const failureItems = [];
+    for (let i = 0; i < arr.length; ++i) {
+        failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+    }
+    // Only change code above this line
+
+    return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
+//=================================
+const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return { name, age, gender };
+    // Only change code above this line
+};
+
+//=================================
+// Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+        this.gear = newGear;
+    }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
