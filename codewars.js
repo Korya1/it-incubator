c0956219e08d758d8d0c7dc19b28ed8fea3b9a7c
Maksym <gurinmaxim11@gmail.com>
2022-03-22 11:04:00 +0200

    3(codewars)

--- codewars.js
+++ codewars.js
@@ -62,3 +62,34 @@ describe("Basic tests", () => {
         as.strictEqual(usdcny(465), '3138.75 Chinese Yuan');
     });
 });
+
+//=======================================
+
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+//=======================================
+
+function doubleChar(str) {
+
+    let newStr = "";
+    for (let i = 0; i < str.length; i++) {
+        newStr += str[i] += str[i];
+    }
+    return newStr;
+}
+
+describe("doubleChar", function () {
+    it("works for some examples", function () {
+        Test.assertEquals(doubleChar("abcd"), "aabbccdd");
+        Test.assertEquals(doubleChar("Adidas"), "AAddiiddaass");
+        Test.assertEquals(doubleChar("1337"), "11333377");
+        Test.assertEquals(doubleChar("illuminati"), "iilllluummiinnaattii");
+        Test.assertEquals(doubleChar("123456"), "112233445566");
+        Test.assertEquals(doubleChar("%^&*("), "%%^^&&**((");
+    });
+});
