8d65f97a68cdbeff073c5813ebf37cbad3c33d4f
Maksym <gurinmaxim11@gmail.com>
2022-03-23 17:42:40 +0200

    4(codewars)

--- codewars.js
+++ codewars.js
@@ -34,12 +34,14 @@ describe("Test Cases", function () {
 
 //=======================================
 
-// Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
+// Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer
+// , and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
 
 // Examples (Input -> Output)
 // * 15  -> '101.25 Chinese Yuan'
 // * 465 -> '3138.75 Chinese Yuan'
-// The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers should be represented as a string with 2 decimal places. (e.g. "21.00" NOT "21.0" or "21")
+// The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers should be represented as a string with
+// 2 decimal places. (e.g. "21.00" NOT "21.0" or "21")
 
 //=======================================
 
@@ -93,3 +95,60 @@ describe("doubleChar", function () {
         Test.assertEquals(doubleChar("%^&*("), "%%^^&&**((");
     });
 });
+
+//=======================================
+
+/*Ask a small girl - "How old are you?". She always says strange things... Lets help her!
+
+For correct answer program should return int from 0 to 9.
+
+Assume test input string always valid and may look like "1 year old" or "5 years old", etc.. The first char is number
+only.*/
+
+//=======================================
+
+function getAge(inputString) {
+    return parseInt(inputString);
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Basic tests", () => {
+    it("Testing for fixed tests", () => {
+        assert.strictEqual(getAge("4 years old"), 4);
+    })
+})
+
+//=======================================
+
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]*/
+
+//=======================================
+
+function maps(x) {
+    let result = [];
+    for (let i = 0; i < x.length; ++i) {
+        result.push(x[i] * 2);
+    }
+    return result;
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Basic tests", () => {
+    it("Fixed tests", () => {
+        assert.deepEqual(maps([1, 2, 3]), [2, 4, 6]);
+        assert.deepEqual(maps([4, 1, 1, 1, 4]), [8, 2, 2, 2, 8]);
+        assert.deepEqual(maps([2, 2, 2, 2, 2, 2]), [4, 4, 4, 4, 4, 4]);
+    });
+});
+
