385c542a30147374b56c297431ffdd89cef9e97e
Maksym <gurinmaxim11@gmail.com>
2022-03-24 18:03:49 +0200

    7(codewars)

--- codewars.js
+++ codewars.js
@@ -223,3 +223,23 @@ describe("Tests", () => {
 
 //=======================================
 
+/*
+Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+spacify("hello world") // returns "h e l l o   w o r l d"
+*/
+
+//=======================================
+
+function spacify(str) {
+    return str.split('').join(' ');
+}
+
+const {assert} = require("chai");
+
+describe("spacify", function() {
+    it("basic tests", function() {
+        assert.equal(spacify('hello world'),'h e l l o   w o r l d');
+        assert.equal(spacify('12345'),'1 2 3 4 5');
+    });
+});
