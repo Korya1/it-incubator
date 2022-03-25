6e5461c4d6f2885ec9773dbb0cb2884a402c0b9b
Maksym <gurinmaxim11@gmail.com>
2022-03-25 20:53:34 +0200

    8(codewars)

--- codewars.js
+++ codewars.js
@@ -243,3 +243,37 @@ describe("spacify", function() {
         assert.equal(spacify('12345'),'1 2 3 4 5');
     });
 });
+
+//=======================================
+
+/*
+You must implement a function that returns the difference between the largest and the smallest value in a given list /
+array (lst) received as the parameter.
+
+lst contains integers, that means it may contain some negative numbers
+if lst is empty or contains a single element, return 0
+lst is not sorted
+[1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+[1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+*/
+
+//=======================================
+
+function maxDiff(list) {
+    if (list.length) {
+        let max = Math.max(...list);
+        let min = Math.min(...list);
+        return max - min;
+    }
+    return 0;
+};
+
+describe('Basic tests', function() {
+
+    Test.assertEquals(maxDiff([0, 1, 2, 3, 4, 5, 6]), 6);
+    Test.assertEquals(maxDiff([-0, 1, 2, -3, 4, 5, -6]), 11);
+    Test.assertEquals(maxDiff([0, 1, 2, 3, 4, 5, 16]), 16);
+    Test.assertEquals(maxDiff([16]), 0);
+    Test.assertEquals(maxDiff([]), 0);
+
+});
