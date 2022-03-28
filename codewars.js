96708251a4129159309acb2f7fc7755a6c49b1ed
Maksym <gurinmaxim11@gmail.com>
2022-03-28 14:15:16 +0300

    10(codewars)

--- codewars.js
+++ codewars.js
@@ -351,3 +351,37 @@ function dotCalculator(equation) {
             doTest(". - .", "");
         });
     });
+
+//=======================================
+    /*
+    Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its
+    value.
+
+        Examples
+    explode("312")
+    should return :
+
+    "333122"
+    explode("102269")
+    should return :
+
+    "12222666666999999999"*/
+
+//=======================================
+
+    function explode(s) {
+        return s.split('').map(n => n.repeat(parseInt(n))).join('');
+    }
+
+    const assert = require('chai').assert;
+    describe("Example tests", () => {
+        const tests = [
+            ["312", "333122"],
+            ["102269", "12222666666999999999"],
+            ["0", ""],
+            ["000", ""],
+            ["123", "122333"]
+        ];
+        for (const [input, expected] of tests)
+            it(JSON.stringify(input), () => assert.strictEqual(explode(input), expected));
+    });
